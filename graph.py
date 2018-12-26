c01851b1fdf3a243b77421f37b600c316240a1ce
Simone Cimarelli <aquilairreale@ymail.com>
2018-12-27 00:07:08 +0100

    Remove unneeded shebangs

--- graph.py
+++ graph.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 
   ############################################################################
   # Diplobot - play Diplomacy through Telegram                               #
