2e3342e2518434b21b2d45de04e364b521b7c7a8
Simone Cimarelli <aquilairreale@ymail.com>
2020-08-11 21:17:38 +0200

    First sqlalchemy draft

--- database.py
+++ database.py
@@ -18,164 +18,297 @@
   ############################################################################
 
 
-from pathlib import Path
-import sqlite3
-
-from utils import qmarks
-from orders import Order
-
-
-schema = """
-CREATE TABLE games (
-    id INTEGER NOT NULL PRIMARY KEY,
-    start_date INTEGER NOT NULL,
-    game_date INTEGER NOT NULL,
-    state TEXT NOT NULL
-);
-CREATE TABLE players (
-    id INTEGER NOT NULL,
-    game_id INTEGER NOT NULL REFERENCES games(id) ON DELETE CASCADE,
-    nation TEXT NOT NULL,
-    ready BOOL NOT NULL,
-    PRIMARY KEY (id, game_id),
-    UNIQUE (game_id, nation)
-);
-CREATE TABLE units (
-    game_id INTEGER NOT NULL REFERENCES games(id) ON DELETE CASCADE,
-    terr TEXT NOT NULL,
-    type TEXT NOT NULL,
-    owner INTEGER NOT NULL REFERENCES players(id),
-    PRIMARY KEY (game_id, terr)
-);
-CREATE TABLE centers (
-    game_id INTEGER NOT NULL REFERENCES games(id) ON DELETE CASCADE,
-    terr TEXT NOT NULL,
-    player_id INTEGER NOT NULL REFERENCES players(id),
-    PRIMARY KEY (game_id, terr)
-);
-CREATE TABLE orders (
-    game_id INTEGER NOT NULL,
-    player_id INTEGER NOT NULL,
-    type TEXT NOT NULL,
-    unit TEXT NOT NULL,
-    terr TEXT NOT NULL,
-    orig TEXT,
-    targ TEXT,
-    coast TEXT,
-    viac BOOL,
-    PRIMARY KEY (game_id, player_id, terr),
-    FOREIGN KEY (game_id, player_id)
-        REFERENCES players(game_id, id) ON DELETE CASCADE
-);
-"""
+from sqlalchemy import Column, Boolean, Integer, String, ForeignKey, UniqueConstraint
+from sqlalchemy.orm import relationship, validates
+from sqlalchemy.orm.collections import attribute_mapped_collection
+from sqlalchemy.ext.declarative import declarative_base
 
 
-def load_schema():
-    c = db.cursor()
-    for stmt in schema.split(";"):
-        if stmt := stmt.strip():
-            c.execute(stmt)
-    db.commit()
+ORMBase = declarative_base()
 
 
-def enable_foreign_keys():
-    c = db.cursor()
-    c.execute("PRAGMA foreign_keys = ON")
-    db.commit()
-    c.execute("PRAGMA foreign_keys")
-    resp = c.fetchone()
-    if resp is None or resp != (1,):
-        raise RuntimeError("sqlite3 implementation doesn't support foreign keys")
+class Game(ORMBase):
+    __tablename__ = "games"
+
+    id = Column(Integer, primary_key=True)
+    start_date = Column(Integer, nullable=False)
+    game_date = Column(Integer, nullable=False)
+    state = Column(String, nullable=False)
+
+    players = relationship(
+            "Player",
+            back_populates="game",
+            collection_class=attribute_mapped_collection("nation"))
+    units = relationship("Unit", back_populates="game")
+    centers = relationship("Center", back_populates="game")
+
+    @validates("state")
+    def validate_state(self, key, s):
+        return s # TODO
+
+    def __init__(self, id, start_date, game_date=None, state="TODO"): # TODO
+        self.id = id
+        self.start_date = start_date
+        self.game_date = game_date if game_date is not None else start_date
+        self.state = state
+
+    def __repr__(self):
+        return (f"Game("
+                f"{repr(self.id)}, "
+                f"{repr(self.start_date)}, "
+                f"{repr(self.game_date)}, "
+                f"{repr(self.state)})")
 
 
-def disable_foreign_keys():
-    c = db.cursor()
-    c.execute("PRAGMA foreign_keys = OFF")
-    db.commit()
+class User(ORMBase):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    cur_game_id = Column(Integer, ForeignKey("games.id"))
+
+    players = relationship("Player", back_populates="user")
+    cur_game = relationship("Game")
+
+    def __init__(self, id):
+        self.id = id
+
+    def __repr__(self):
+        return f"User({repr(self.id)})"
 
 
-def get_player_nation(game_id, player_id, nation):
-    raise NotImplementedError
+class Player(ORMBase):
+    __tablename__ = "players"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    game_id = Column(Integer, ForeignKey("games.id"), nullable=False)
+    nation = Column(String, nullable=False)
+    ready = Column(Boolean, nullable=False)
+
+    __table_args__ = (
+        UniqueConstraint("game_id", "nation"),
+        UniqueConstraint("user_id", "game_id")
+    )
+
+    user = relationship("User", back_populates="players")
+    game = relationship("Game", back_populates="players")
+    units = relationship("Unit", back_populates="owner")
+    centers = relationship("Center", back_populates="owner")
+
+    @validates("state")
+    def validate_state(self, key, s):
+        return s # TODO
+
+    def __init__(self, id, nation):
+        self.id = id
+        self.nation = nation
+        self.ready = False
+
+    def __repr__(self):
+        return (f"Player("
+                f"{repr(self.id)}, "
+                f"{repr(self.nation)})")
 
 
-def set_ready(game_id, player_id, ready=True):
-    c = db.cursor()
-    c.execute("UPDATE players SET ready = ?", (ready,))
-    db.commit()
+class Unit(ORMBase):
+    __tablename__ = "units"
+
+    game_id = Column(Integer, ForeignKey("games.id"), primary_key=True)
+    terr = Column(String, primary_key=True)
+    type = Column(String, nullable=False)
+    owner_id = Column(Integer, ForeignKey("players.id"), nullable=False)
+
+    game = relationship("Game", back_populates="units")
+    owner = relationship("Player", back_populates="units")
+
+    def __init__(self, terr, type):
+        self.terr = terr
+        self.type = type
+
+    def __repr__(self):
+        return (f"Unit("
+                f"{repr(self.terr)}, "
+                f"{repr(self.type)})")
 
 
-def all_ready(game_id):
-    c = db.cursor()
-    c.execute(
-        "SELECT COUNT(*) "
-        "FROM players "
-        "WHERE game_id = ? "
-        "AND ready IS FALSE",
-        (game_id,))
-    (not_ready,) = c.fetchone()
-    return not_ready == 0
+class Center(ORMBase):
+    __tablename__ = "centers"
+
+    game_id = Column(Integer, ForeignKey("games.id"), primary_key=True)
+    terr = Column(String, primary_key=True)
+    owner_id = Column(Integer, ForeignKey("players.id"))
+
+    __table_args__ = (UniqueConstraint("game_id", "terr"),)
+
+    game = relationship("Game", back_populates="centers")
+    owner = relationship("Player", back_populates="centers")
+
+    def __init__(self, terr):
+        self.terr = terr
+
+    def __repr__(self):
+        return (f"Center("
+                f"{repr(self.terr)})")
 
 
-def delete_orders(game_id, player_id, *terrs):
-    c = db.cursor()
-    c.execute(
-        f"DELETE FROM orders "
-        f"WHERE game_id = ? "
-        f"AND player_id = ? "
-        f"AND terr IN ({qmarks(len(terrs))})",
-        (game_id, player_id, *(str(t) for t in terrs)))
-    db.commit()
-
-
-def store_orders(game_id, player_id, *orders):
-    c = db.cursor()
-    for order in orders:
-        terr, *rest = order.to_db_tuple()
-        c.execute(
-            "DELETE FROM orders "
-            "WHERE game_id = ? "
-            "AND player_id = ?"
-            "AND terr = ?",
-            (game_id, player_id, str(terr)))
-        c.execute(
-            "INSERT INTO orders"
-            "(game_id, player_id, type, unit, terr, orig, targ, coast, viac) "
-            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-            (game_id, player_id, terr, *db_tuple))
-    db.commit()
-
-
-def retrieve_orders(game_id, player_id=None, order_types=None):
-    order_types = [
-        (o.typestr if issubclass(o, Order) else o)
-        for o in order_types
-    ]
-
-    query = (
-        "SELECT type, unit, terr, orig, targ, coast, viac "
-        "FROM orders "
-        "WHERE game_id=?")
-    params = (game_id,)
-
-    if player_id is not None:
-        query += " AND player_id=?"
-        params += (player_id,)
-
-    if order_types is not None:
-        query += f" AND type IN ({qmarks(len(order_types))})"
-        params += tuple(order_types)
-
-    c = db.cursor()
-    c.execute(query, params)
-    while db_tuple := c.fetchone():
-        yield Order.from_db_tuple(*db_tuple)
-
-
-db_file = "diplobot.db"
-
-db_exists = Path(db_file).exists()
-db = sqlite3.connect(db_file)
-enable_foreign_keys()
-
-if not db_exists:
-    load_schema()
+#from pathlib import Path
+#import sqlite3
+#
+#from utils import qmarks
+#from orders import Order
+#
+#
+#schema = """
+#CREATE TABLE games (
+#    id INTEGER NOT NULL PRIMARY KEY,
+#    start_date INTEGER NOT NULL,
+#    game_date INTEGER NOT NULL,
+#    state TEXT NOT NULL
+#);
+#CREATE TABLE players (
+#    id INTEGER NOT NULL,
+#    game_id INTEGER NOT NULL REFERENCES games(id) ON DELETE CASCADE,
+#    nation TEXT NOT NULL,
+#    ready BOOL NOT NULL,
+#    PRIMARY KEY (id, game_id),
+#    UNIQUE (game_id, nation)
+#);
+#CREATE TABLE units (
+#    game_id INTEGER NOT NULL REFERENCES games(id) ON DELETE CASCADE,
+#    terr TEXT NOT NULL,
+#    type TEXT NOT NULL,
+#    owner INTEGER NOT NULL REFERENCES players(id),
+#    PRIMARY KEY (game_id, terr)
+#);
+#CREATE TABLE centers (
+#    game_id INTEGER NOT NULL REFERENCES games(id) ON DELETE CASCADE,
+#    terr TEXT NOT NULL,
+#    player_id INTEGER NOT NULL REFERENCES players(id),
+#    PRIMARY KEY (game_id, terr)
+#);
+#CREATE TABLE orders (
+#    game_id INTEGER NOT NULL,
+#    player_id INTEGER NOT NULL,
+#    type TEXT NOT NULL,
+#    unit TEXT NOT NULL,
+#    terr TEXT NOT NULL,
+#    orig TEXT,
+#    targ TEXT,
+#    coast TEXT,
+#    viac BOOL,
+#    PRIMARY KEY (game_id, player_id, terr),
+#    FOREIGN KEY (game_id, player_id)
+#        REFERENCES players(game_id, id) ON DELETE CASCADE
+#);
+#"""
+#
+#
+#def load_schema():
+#    c = db.cursor()
+#    for stmt in schema.split(";"):
+#        if stmt := stmt.strip():
+#            c.execute(stmt)
+#    db.commit()
+#
+#
+#def enable_foreign_keys():
+#    c = db.cursor()
+#    c.execute("PRAGMA foreign_keys = ON")
+#    db.commit()
+#    c.execute("PRAGMA foreign_keys")
+#    resp = c.fetchone()
+#    if resp is None or resp != (1,):
+#        raise RuntimeError("sqlite3 implementation doesn't support foreign keys")
+#
+#
+#def disable_foreign_keys():
+#    c = db.cursor()
+#    c.execute("PRAGMA foreign_keys = OFF")
+#    db.commit()
+#
+#
+#def get_player_nation(game_id, player_id, nation):
+#    raise NotImplementedError
+#
+#
+#def set_ready(game_id, player_id, ready=True):
+#    c = db.cursor()
+#    c.execute("UPDATE players SET ready = ?", (ready,))
+#    db.commit()
+#
+#
+#def all_ready(game_id):
+#    c = db.cursor()
+#    c.execute(
+#        "SELECT COUNT(*) "
+#        "FROM players "
+#        "WHERE game_id = ? "
+#        "AND ready IS FALSE",
+#        (game_id,))
+#    (not_ready,) = c.fetchone()
+#    return not_ready == 0
+#
+#
+#def delete_orders(game_id, player_id, *terrs):
+#    c = db.cursor()
+#    c.execute(
+#        f"DELETE FROM orders "
+#        f"WHERE game_id = ? "
+#        f"AND player_id = ? "
+#        f"AND terr IN ({qmarks(len(terrs))})",
+#        (game_id, player_id, *(str(t) for t in terrs)))
+#    db.commit()
+#
+#
+#def store_orders(game_id, player_id, *orders):
+#    c = db.cursor()
+#    for order in orders:
+#        terr, *rest = order.to_db_tuple()
+#        c.execute(
+#            "DELETE FROM orders "
+#            "WHERE game_id = ? "
+#            "AND player_id = ?"
+#            "AND terr = ?",
+#            (game_id, player_id, str(terr)))
+#        c.execute(
+#            "INSERT INTO orders"
+#            "(game_id, player_id, type, unit, terr, orig, targ, coast, viac) "
+#            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+#            (game_id, player_id, terr, *db_tuple))
+#    db.commit()
+#
+#
+#def retrieve_orders(game_id, player_id=None, order_types=None):
+#    order_types = [
+#        (o.typestr if issubclass(o, Order) else o)
+#        for o in order_types
+#    ]
+#
+#    query = (
+#        "SELECT type, unit, terr, orig, targ, coast, viac "
+#        "FROM orders "
+#        "WHERE game_id=?")
+#    params = (game_id,)
+#
+#    if player_id is not None:
+#        query += " AND player_id=?"
+#        params += (player_id,)
+#
+#    if order_types is not None:
+#        query += f" AND type IN ({qmarks(len(order_types))})"
+#        params += tuple(order_types)
+#
+#    c = db.cursor()
+#    c.execute(query, params)
+#    while db_tuple := c.fetchone():
+#        yield Order.from_db_tuple(*db_tuple)
+#
+#
+#db_file = "diplobot.db"
+#
+#db_exists = Path(db_file).exists()
+#db = sqlite3.connect(db_file)
+#enable_foreign_keys()
+#
+#if not db_exists:
+#    load_schema()
